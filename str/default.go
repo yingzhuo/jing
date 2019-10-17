a6340e5a13be859b58676392f4483fe2ed3c85b9
yingzhuo <yingzhor@gmail.com>
2019-10-17 15:47:59 +0800

    2019-10-17 15:47:59

---
+++ str/default.go
@@ -0,0 +1,25 @@
+/**********************************************************************************************************************
+ *      _ _
+ *     | (_)_ __   __ _
+ *  _  | | | '_ \ / _` |
+ * | |_| | | | | | (_| |
+ *  \___/|_|_| |_|\__, |                        https://github.com/yingzhuo/jing
+ *                |___/
+ **********************************************************************************************************************/
+package str
+
+func DefaultIfEmpty(s string, defaultValue string) string {
+	if IsEmpty(s) {
+		return defaultValue
+	} else {
+		return s
+	}
+}
+
+func DefaultIfBlank(s string, defaultValue string) string {
+	if IsBlank(s) {
+		return defaultValue
+	} else {
+		return s
+	}
+}
