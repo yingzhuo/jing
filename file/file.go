146a24efcfc6157b727e20138c22cf1bd79cfda9
yingzhuo <yingzhor@gmail.com>
2019-10-18 09:45:05 +0800

    2019-10-18 09:45:05

---
+++ file/file.go
@@ -0,0 +1,40 @@
+/**********************************************************************************************************************
+ *      _ _
+ *     | (_)_ __   __ _
+ *  _  | | | '_ \ / _` |
+ * | |_| | | | | | (_| |
+ *  \___/|_|_| |_|\__, |                        https://github.com/yingzhuo/jing
+ *                |___/
+ **********************************************************************************************************************/
+package file
+
+import "os"
+
+func Exists(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
+}
+
+func IsDir(name string) bool {
+	if info, err := os.Stat(name); err != nil {
+		panic(err)
+	} else {
+		return info.IsDir()
+	}
+}
+
+func IsFile(name string) bool {
+	if info, err := os.Stat(name); err != nil {
+		panic(err)
+	} else {
+		return !info.IsDir()
+	}
+}
+
+func Size(name string) int64 {
+	if info, err := os.Stat(name); err != nil {
+		panic(err)
+	} else {
+		return info.Size()
+	}
+}
