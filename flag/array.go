0eb0e13fbbf76aa41d5c91b759d24b567337da8f
yingzhuo <yingzhor@gmail.com>
2019-10-23 07:27:47 +0800

    2019-10-23 07:27:47

--- flag/array.go
+++ flag/array.go
@@ -8,7 +8,10 @@
  **********************************************************************************************************************/
 package flag
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 type ArrayFlags []string // flag.Value interface
 
@@ -19,4 +22,28 @@ func (i *ArrayFlags) Set(value string) error {
 
 func (i *ArrayFlags) String() string {
 	return strings.Join(*i, ",")
+}
+
+func (i *ArrayFlags) Size() int {
+	return len(*i)
+}
+
+func (i *ArrayFlags) IsEmpty() bool {
+	return i.Size() == 0
+}
+
+func (i *ArrayFlags) Map(fn func(string) string) ArrayFlags {
+	ret := ArrayFlags{}
+	for _, v := range *i {
+		ret = append(ret, fn(v))
+	}
+	return ret
+}
+
+func (i *ArrayFlags) MapTrim() ArrayFlags {
+	return i.Map(func(s string) string {
+		s = strings.TrimLeftFunc(s, unicode.IsSpace)
+		s = strings.TrimRightFunc(s, unicode.IsSpace)
+		return s
+	})
 }
