2d182c5c17d4c0187e44dcd995a58a0d2d505bcb
yingzhuo <yingzhor@gmail.com>
2019-11-09 07:26:33 +0800

    2019-11-09 07:26:32

--- conv/conv.go
+++ conv/conv.go
@@ -59,7 +59,7 @@ func ToInt(a interface{}) (int, error) {
 	case uint64:
 		return int(i), nil
 	case int:
-		return int(i), nil
+		return i, nil
 	case int8:
 		return int(i), nil
 	case int16:
@@ -90,6 +90,138 @@ func ToInt(a interface{}) (int, error) {
 	}
 }
 
+func ToInt8(a interface{}) (int8, error) {
+	switch i := a.(type) {
+	case uint:
+		return int8(i), nil
+	case uint8:
+		return int8(i), nil
+	case uint16:
+		return int8(i), nil
+	case uint32:
+		return int8(i), nil
+	case uint64:
+		return int8(i), nil
+	case int:
+		return int8(i), nil
+	case int8:
+		return i, nil
+	case int16:
+		return int8(i), nil
+	case int32:
+		return int8(i), nil
+	case int64:
+		return int8(i), nil
+	case float32:
+		return int8(i), nil
+	case float64:
+		return int8(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return int8(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to int8")
+	}
+}
+
+func ToInt16(a interface{}) (int16, error) {
+	switch i := a.(type) {
+	case uint:
+		return int16(i), nil
+	case uint8:
+		return int16(i), nil
+	case uint16:
+		return int16(i), nil
+	case uint32:
+		return int16(i), nil
+	case uint64:
+		return int16(i), nil
+	case int:
+		return int16(i), nil
+	case int8:
+		return int16(i), nil
+	case int16:
+		return i, nil
+	case int32:
+		return int16(i), nil
+	case int64:
+		return int16(i), nil
+	case float32:
+		return int16(i), nil
+	case float64:
+		return int16(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return int16(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to int16")
+	}
+}
+
+func ToInt32(a interface{}) (int32, error) {
+	switch i := a.(type) {
+	case uint:
+		return int32(i), nil
+	case uint8:
+		return int32(i), nil
+	case uint16:
+		return int32(i), nil
+	case uint32:
+		return int32(i), nil
+	case uint64:
+		return int32(i), nil
+	case int:
+		return int32(i), nil
+	case int8:
+		return int32(i), nil
+	case int16:
+		return int32(i), nil
+	case int32:
+		return i, nil
+	case int64:
+		return int32(i), nil
+	case float32:
+		return int32(i), nil
+	case float64:
+		return int32(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return int32(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to int32")
+	}
+}
+
 func ToInt64(a interface{}) (int64, error) {
 	switch i := a.(type) {
 	case uint:
@@ -111,7 +243,7 @@ func ToInt64(a interface{}) (int64, error) {
 	case int32:
 		return int64(i), nil
 	case int64:
-		return int64(i), nil
+		return i, nil
 	case float32:
 		return int64(i), nil
 	case float64:
@@ -127,10 +259,230 @@ func ToInt64(a interface{}) (int64, error) {
 		if err != nil {
 			return 0, err
 		} else {
-			return int64(n), nil
+			return n, nil
 		}
 	default:
 		return 0, fmt.Errorf("can not conv to int64")
+	}
+}
+
+func ToUint(a interface{}) (uint, error) {
+	switch i := a.(type) {
+	case uint:
+		return i, nil
+	case uint8:
+		return uint(i), nil
+	case uint16:
+		return uint(i), nil
+	case uint32:
+		return uint(i), nil
+	case uint64:
+		return uint(i), nil
+	case int:
+		return uint(i), nil
+	case int8:
+		return uint(i), nil
+	case int16:
+		return uint(i), nil
+	case int32:
+		return uint(i), nil
+	case int64:
+		return uint(i), nil
+	case float32:
+		return uint(i), nil
+	case float64:
+		return uint(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return uint(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to uint")
+	}
+}
+
+func ToUint8(a interface{}) (uint8, error) {
+	switch i := a.(type) {
+	case uint:
+		return uint8(i), nil
+	case uint8:
+		return i, nil
+	case uint16:
+		return uint8(i), nil
+	case uint32:
+		return uint8(i), nil
+	case uint64:
+		return uint8(i), nil
+	case int:
+		return uint8(i), nil
+	case int8:
+		return uint8(i), nil
+	case int16:
+		return uint8(i), nil
+	case int32:
+		return uint8(i), nil
+	case int64:
+		return uint8(i), nil
+	case float32:
+		return uint8(i), nil
+	case float64:
+		return uint8(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return uint8(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to uint8")
+	}
+}
+
+func ToUint16(a interface{}) (uint16, error) {
+	switch i := a.(type) {
+	case uint:
+		return uint16(i), nil
+	case uint8:
+		return uint16(i), nil
+	case uint16:
+		return i, nil
+	case uint32:
+		return uint16(i), nil
+	case uint64:
+		return uint16(i), nil
+	case int:
+		return uint16(i), nil
+	case int8:
+		return uint16(i), nil
+	case int16:
+		return uint16(i), nil
+	case int32:
+		return uint16(i), nil
+	case int64:
+		return uint16(i), nil
+	case float32:
+		return uint16(i), nil
+	case float64:
+		return uint16(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return uint16(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to uint16")
+	}
+}
+
+func ToUint32(a interface{}) (uint32, error) {
+	switch i := a.(type) {
+	case uint:
+		return uint32(i), nil
+	case uint8:
+		return uint32(i), nil
+	case uint16:
+		return uint32(i), nil
+	case uint32:
+		return i, nil
+	case uint64:
+		return uint32(i), nil
+	case int:
+		return uint32(i), nil
+	case int8:
+		return uint32(i), nil
+	case int16:
+		return uint32(i), nil
+	case int32:
+		return uint32(i), nil
+	case int64:
+		return uint32(i), nil
+	case float32:
+		return uint32(i), nil
+	case float64:
+		return uint32(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return uint32(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to uint32")
+	}
+}
+
+func ToUint64(a interface{}) (uint64, error) {
+	switch i := a.(type) {
+	case uint:
+		return uint64(i), nil
+	case uint8:
+		return uint64(i), nil
+	case uint16:
+		return uint64(i), nil
+	case uint32:
+		return uint64(i), nil
+	case uint64:
+		return i, nil
+	case int:
+		return uint64(i), nil
+	case int8:
+		return uint64(i), nil
+	case int16:
+		return uint64(i), nil
+	case int32:
+		return uint64(i), nil
+	case int64:
+		return uint64(i), nil
+	case float32:
+		return uint64(i), nil
+	case float64:
+		return uint64(i), nil
+	case bool:
+		if i {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case string:
+		n, err := stringToInt64(i)
+		if err != nil {
+			return 0, err
+		} else {
+			return uint64(n), nil
+		}
+	default:
+		return 0, fmt.Errorf("can not conv to uint64")
 	}
 }
 
